a5c788d3e230086ff4eac4325269dce16eb98858
Marino <48334228+marinofranz@users.noreply.github.com>
2020-12-28 12:03:37 +0000

    uninstall bash script

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker stop portainer-ce
+docker rm portainer-ce
+docker volume rm portainer_data
+
+cd /home/docker
+
+echo "Installation removed!"
+echo "-----"
+echo "If you would NOT like to delete Portainer data, please exit out of this script within 10 seconds"
+
+sleep 10
+
+sudo rm -r portainer
